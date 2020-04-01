76da1076355aac8d7a1b7a615b2ade5ea4cd0e52
Doug Knight <dougbknight@gmail.com>
2020-03-31 23:12:08 -0500

    add basic parts

--- src/stowaway/core.clj
+++ src/stowaway/core.clj
@@ -1,6 +1,106 @@
-(ns stowaway.core)
+(ns stowaway.core
+  (:refer-clojure :exclude [update])
+  (:require [clojure.tools.logging :as log]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defprotocol Storage
+  "This protocol defines the abstract CRUD operations to be used
+  by the application. This abstraction can be implemented by whatever
+  specific storage you want to use."
+  (create
+    [this model]
+    "Creates a new model record")
+  (select
+    [this criteria options]
+    "Selects model records matching the specified criteria")
+  (update
+    [this model]
+    "Updates the specified model record")
+  (delete
+    [this model]
+    "Deletes the specified model record")
+
+  ; Data integrity utilities
+  ; ------------------------
+
+  (with-transaction
+    [this func]
+    "Executes the specified function which expects a single argument,
+    which is a transacted storage instance"))
+
+(def ^:private storage-handlers (atom []))
+
+(defn register-strategy
+  "Registers a data storage stragey so that it can be
+  used in an application.
+
+  The single argument is a function that will be
+  called and given the configuration for the storage
+  strategy. If the strategy is able to initialize itself
+  with the given configuration, it returns the reified
+  strategy. Otherwise it returns nil."
+  [f]
+  (swap! storage-handlers conj f))
+
+; If the given storage configuration is a reified storage strategy, return it
+(register-strategy #(when (satisfies? Storage %) %))
+
+(defn reify-storage
+  "Searches registered strategies for one that is able to initialize itself
+  with the given confiration.
+
+  If the given configuration implements Storage, it is returned. This
+  allows a method that expects a storage configuration to receive a
+  reified storage strategy also."
+  [config]
+  (or (some #(% config) @storage-handlers)
+      (throw (RuntimeException. "Unrecognized storage specification."))))
+
+(defmacro with-storage
+  "Binds a reified storage strategy to the
+  specified var.
+
+  (with-storage [storage config]
+    (storage/update my-model))"
+  [binding & body]
+  `(let [s# (reify-storage ~(second binding))
+         f# (fn* [~(first binding)] ~@body)]
+     (f# s#)))
+
+(defmacro with-transacted-storage
+  "Evaluates the body in the context of a transaction using the configured
+  storage mechanism.
+  (transacted-storage [storage config]
+  ...do stuff with the storage...)"
+  [binding & body]
+  `(let [s# (reify-storage ~(second binding))
+         f# (fn* [~(first binding)] ~@body)]
+     (with-transaction s# f#)))
+
+(defn- ensure-tag
+  [meta-data model-type]
+  (when-let [existing (get-in meta-data [::type])]
+    (when-not (= existing model-type)
+      (log/warnf "Overwriting model tag %s with %s" existing model-type)))
+  (assoc meta-data ::type model-type))
+
+(defn tag
+  "Given two arguments returns the first argument (the model) with
+  updated metadata that can be used to determine how the model
+  should be stored.
+
+  Given one argument, returns the type identifier from the metadata.
+
+  If the model already has a tag, and that tag is different that the
+  second argument, a warning is written to the log."
+  ([model]
+   (-> model
+       meta
+       ::type))
+  ([model model-type]
+   (vary-meta model ensure-tag model-type)))
+
+(defn tagged?
+  "Returns true if the specified model is tagged
+  with the specified value."
+  [model model-type]
+  (= model-type (tag model)))
--- test/stowaway/core_test.clj
+++ test/stowaway/core_test.clj
@@ -1,7 +1,29 @@
 (ns stowaway.core-test
-  (:require [clojure.test :refer :all]
-            [stowaway.core :refer :all]))
+  (:require [clojure.test :refer [deftest is]]
+            [clojure.pprint :refer [pprint]]
+            [stowaway.core :as sto]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest tag-a-model
+  (is (= :models-ns/user
+         (-> {}
+             (sto/tag :models-ns/user)
+             sto/tag))))
+
+(deftest check-a-model-tag
+  (let [model (sto/tag {} :models-ns/user)]
+  (is (sto/tagged? model :models-ns/user))
+  (is (not (sto/tagged? model :models-ns/phone-number)))))
+
+(deftype TestStorage [config]
+  sto/Storage
+  (select [_ criteria options] (pprint {:select criteria :options options}))
+  (create [_ model] (pprint {:create model}))
+  (update [_ model] (pprint {:update model}))
+  (delete [_ model] (pprint {:delete model})))
+
+(deftest register-a-storage-strategy
+  (sto/register-strategy (fn [config]
+                           (when (re-find #"\Atest" config)
+                             (TestStorage. config))))
+  (sto/with-storage [s "test://blah-blah"]
+    (is (instance? TestStorage s))))
