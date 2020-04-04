1c114efaf55836fe2bfc848f08553d797af7f7cd
Doug Knight <dougbknight@gmail.com>
2020-04-04 13:59:32 -0500

    fix library description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject stowaway "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject stowaway "0.1.1-SNAPSHOT"
+  :description "Library for abstracting data storage from business logic"
+  :url "https://github.com/dgknght/stowaway"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
