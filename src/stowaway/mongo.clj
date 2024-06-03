a0fba6007f0df769b746a0483a53646116291edb
Doug Knight <dougbknight@gmail.com>
2024-06-03 16:05:31 -0500

    refine mongo criteria logic

--- src/stowaway/mongo.clj
+++ src/stowaway/mongo.clj
@@ -1,27 +1,51 @@
 (ns stowaway.mongo
-  #_(:require [stowaway.graph :as g]
-            [stowaway.inflection :refer [singular]]
+  (:require [clojure.pprint :refer [pprint]]
+            [camel-snake-kebab.core :refer [->snake_case_keyword]]
+            [stowaway.graph :as g]
+            [stowaway.inflection :refer [singular
+                                         plural]]
             [stowaway.criteria :refer [namespaces
                                        extract-ns]]))
 
-#_(defn- extract-collections
+(defn- extract-collections
+  "Give a criteria, return a vector of namespaces tranlated
+  into collection names as keywords"
   [criteria]
-  (map identity (namespaces criteria)))
+  (map (comp keyword
+             plural)
+       (namespaces criteria)))
 
-#_(defn- match
+(defn- match
   [criteria]
-  {:$match :x})
+  {:$match (update-keys criteria ->snake_case_keyword)})
 
-#_(defn- lookup-and-match
-  [collection relationships])
+(defn- lookup-and-match
+  [[n1 n2 :as edge] relationships]
+  (let [[_ c2] (some relationships
+                      [edge
+                       (reverse edge)])
+        from (name n2)
+        [local foreign] (if (= c2 n2)
+                          ["_id" (str (name n1) "_id")]
+                          [(str (singular (name n2)) "_id") "_id"])]
+    [{:$lookup {:from from
+               :as from
+               :localField local
+               :foreignField foreign}}]))
+
+(defn- path->stages
+  [path relationships]
+  (->> path
+       (partition 2 1)
+       (mapcat #(lookup-and-match % relationships))))
 
 (defn criteria->aggregation
-  [_criteria {:keys [_collection _relationships]}]
-  #_(let [paths (g/shortest-paths collection
+  [criteria {:keys [collection relationships]}]
+  (let [paths (g/shortest-paths collection
                                 (extract-collections criteria)
                                 relationships)]
     (cons (-> criteria
               (extract-ns (singular collection))
               match)
-          (map #(lookup-and-match % relationships)
-               (rest paths)))))
+          (mapcat #(path->stages % relationships)
+               paths))))
--- test/stowaway/mongo_test.clj
+++ test/stowaway/mongo_test.clj
@@ -2,7 +2,7 @@
   (:require [clojure.test :refer [deftest is]]
             [stowaway.mongo :as m]))
 
-(deftest convert-a-criteria-into-an-aggregation-pipeline
+(deftest convert-criteria-into-an-aggregation-pipeline
   (is (= [{:$match {:purchase_date "2020-01-01"}}
           {:$lookup {:from "users"
                     :as "users"
@@ -11,5 +11,5 @@
           {:$match {:users.first_name "John"}}]
          (m/criteria->aggregation {:order/purchase-date "2020-01-01"
                                    :user/first-name "John"}
-                                  {:target :user
+                                  {:collection :orders
                                    :relationships #{[:users :orders]}}))))
