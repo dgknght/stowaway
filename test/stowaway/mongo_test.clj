ca6ad69706575b17056625fbffd287aa9f8be5cd
Doug Knight <dougbknight@gmail.com>
2024-06-03 19:30:09 -0500

    add downatream test

--- test/stowaway/mongo_test.clj
+++ test/stowaway/mongo_test.clj
@@ -1,9 +1,26 @@
 (ns stowaway.mongo-test
-  (:require [clojure.test :refer [deftest is]]
+  (:require [clojure.test :refer [deftest is testing]]
             [stowaway.mongo :as m]))
 
 (deftest convert-criteria-into-an-aggregation-pipeline
-  (is (= [{:$match {:purchase_date "2020-01-01"}}
+  (testing "simple map with downstream join"
+    (is (= [{:$match {:users.first_name "John"}}
+          {:$lookup {:from "orders"
+                    :as "orders"
+                    :localField "_id"
+                    :foreignField "user_id"}}
+            {:$match {:purchase_date "2020-01-01"}}]
+         (m/criteria->aggregation {:order/purchase-date "2020-01-01"
+                                   :user/first-name "John"}
+                                  {:collection :users
+                                   :relationships #{[:users :orders]}}))))
+  ; by "upstream", we mean that the relationship is defined
+  ; users -> orders, or a user has many orders and an order belongs
+  ; to a user. However, the target collection is :orders, so navigating
+  ; the graph to users goes against the direction of the primary -> foreign
+  ; relationship
+  (testing "simple map with upstream join"
+    (is (= [{:$match {:purchase_date "2020-01-01"}}
           {:$lookup {:from "users"
                     :as "users"
                     :localField "user_id"
@@ -12,4 +29,4 @@
          (m/criteria->aggregation {:order/purchase-date "2020-01-01"
                                    :user/first-name "John"}
                                   {:collection :orders
-                                   :relationships #{[:users :orders]}}))))
+                                   :relationships #{[:users :orders]}})))))
